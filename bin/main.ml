6fc0dd74352a75926d88522dada5b50663cad04a
Zak Kent <zak33@cornell.edu>
2024-05-15 22:56:28 -0400

    Basic REPL

--- bin/main.ml
+++ bin/main.ml
@@ -2,20 +2,37 @@ open Core
 open Cka
 open SeriesRational
 
-let () =
-  let lhs = Seq (Par (Sym 'a', Sym 'b'), Par (Sym 'c', Sym 'd')) in
-  let rhs = Par (Seq (Sym 'b', Sym 'c'), Seq (Sym 'a', Sym 'd')) in
-  printf "%b\n" @@ Refinement.refines lhs rhs
+let parse s =
+  Option.try_with @@ fun () -> Parser.expr Lexer.read @@ Lexing.from_string s
 
-  (* for i = 1 to 100 do
-    let random = Splittable_random.State.create Random.State.default in
-    let e1 =
-      Quickcheck.Generator.generate SeriesRational.quickcheck_generator
-        ~size:100 ~random
-    in
-    let e2 =
-      Quickcheck.Generator.generate SeriesRational.quickcheck_generator
-        ~size:100 ~random
-    in
-    
-  done *)
+let rec input_term () =
+  printf ">>> %!";
+  let default () =
+    print_endline "Sorry, that is not a valid term. Please try again.";
+    input_term ()
+  in
+  Option.value_or_thunk ~default
+  @@
+  let open Option.Monad_infix in
+  In_channel.input_line In_channel.stdin >>= parse
+
+let () =
+  print_endline "Welcome to my CKA decision procedure!";
+  print_endline
+    "Enter a series-rational (SR) term, then a newline, and then another SR \
+     term.";
+  print_endline
+    {|SR expressions are simply regular expressions extended with parallel composition. For example, "a || b" is the parallel composition of "a" and "b"|};
+  print_endline
+    {|It will tell you whether the first refines the second; that is, whether the first is "more sequential" than the second, as governed by the exchange axiom|};
+  print_endline "Interrupt the process at any time to exit\n";
+  try
+    while true do
+      let s = input_term () in
+      let t = input_term () in
+      if Refinement.refines s t then
+        print_endline "The first term you entered refines the second\n"
+      else
+        print_endline "The first term you entered does not refine the second\n"
+    done
+  with _ -> print_endline "Goodbye!"
